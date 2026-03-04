9abd6f9bd517bfffeab9f3505510a2db98dd7812
Ritesh Kumar <deepakdabas676@gmail.com>
2026-03-04 12:03:43 +0530

    smtp port fix

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 from flask_mail import Mail, Message
 
 app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 2525
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
