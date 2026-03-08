62e2067dbab5419df9ac6bd186f41587ac879886
Ritesh Kumar <deepakdabas676@gmail.com>
2026-03-08 23:15:57 +0530

    improve diagnose UI

--- app.py
+++ app.py
@@ -1425,4 +1425,5 @@ with app.app_context():
 # ================= START SERVER =================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
